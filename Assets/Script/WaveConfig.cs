ed8f2e4fcb7f58d2f355ce92dd81c6136127835d
Alden <aj.molina092@gmail.com>
2019-01-31 21:15:05 -0500

    Added wave configuration and created a new path.

--- Assets/Script/WaveConfig.cs
+++ Assets/Script/WaveConfig.cs
@@ -2,17 +2,35 @@
 using System.Collections.Generic;
 using UnityEngine;
 
-public class WaveConfig : MonoBehaviour
+[CreateAssetMenu(menuName = "Enemy Wave Config")]
+public class WaveConfig : ScriptableObject
 {
-    // Start is called before the first frame update
-    void Start()
+
+    [SerializeField] GameObject enemyPrefab;
+    [SerializeField] GameObject pathPrefab;
+    [SerializeField] float timeBetweenSpawns = 0.5f;
+    [SerializeField] float spawnRandomFactor = 0.3f;
+    [SerializeField] int numberOfEnemies = 5;
+    [SerializeField] float moveSpeed = 2f;
+
+    public GameObject GetEnemyPrefab() { return enemyPrefab; }
+
+    public List<Transform> GetWaypoints()
     {
-        
+        var waveWaypoints = new List<Transform>();
+        foreach (Transform child in pathPrefab.transform)
+        {
+            waveWaypoints.Add(child);
+        }
+        return waveWaypoints;
     }
 
-    // Update is called once per frame
-    void Update()
-    {
-        
-    }
+    public float GetTimeBetweenSpawns() { return timeBetweenSpawns; }
+
+    public float GetSpawnRandomFactor() { return spawnRandomFactor; }
+
+    public int GetNumberOfEnemies() { return numberOfEnemies; }
+
+    public float GetMoveSpeed() { return moveSpeed; }
+
 }
